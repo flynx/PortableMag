842021997695a2490ff80f04e066f978745fc705
Alex A. Naanou <alex.nanou@gmail.com>
2013-02-05 20:22:08 +0400

    minor tweek...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- platform.js
+++ platform.js
@@ -1,7 +1,14 @@
-
+/**********************************************************************
+*
+*
+**********************************************************************/
 
 // PhoneGap...
 if(navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/)){
 	UPDATE_HASH_URL_POSITION = false
 	FULL_HISTORY_ENABLED = false
 }
+
+
+/*********************************************************************/
+// vim:set ts=4 sw=4 :
