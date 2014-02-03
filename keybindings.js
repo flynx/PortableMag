95301411e46a890c275fb7cf049f327745cacff4
Alex A. Naanou <alex.nanou@gmail.com>
2014-02-04 00:23:11 +0400

    started project revival -- updated keyboard.js and split out the bindings...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- keybindings.js
+++ keybindings.js
@@ -1,94 +1,173 @@
 /*********************************************************************/
-// NOTE: use String.fromCharCode(code)...
-// list of keys to be ignored by handler but still handled by the browser...
 
-var keybindings = {
-	/*
-	// global bindings...
-	'*': {
-		title: 'Global',
+var KEYBOARD_CONFIG = {
+	'Global bindings': {
+		doc: 'NOTE: binding priority is the same as the order of sections '+
+			'on this page.',
+		pattern: '*',
+
+		F4: {
+			alt: doc('Close viewer', 
+				function(){ 
+					if(window.require != null){
+						require('nw.gui')
+							.Window.get().close()
+						return false
+					}
+				}),
+		},
+		F5: doc('Full reload viewer', 
+			function(){ 
+				if(window.require != null){
+					require('nw.gui')
+						.Window.get().reload()
+					return false
+				}
+			}),
+		F12: doc('Show devTools', 
+			function(){ 
+				if(window.require != null){
+					require('nw.gui')
+						.Window.get().showDevTools()
+					return false
+				}
+			}),
+	},	
+
+	'.overlay': {
+		title: 'Overlay mode.',
 		doc: '',
 
-		ignore: [
-			116,												//	F5
-			122,												//	F11
-			123,												//	F12
-			8													//	BkSp
-		],
+		ignore: '*',
 
-		// ignore the modifiers (shift, alt, ctrl, caps)...
-		16:		function(){},
-		17:		16,
-		18:		16,
-		20:		16,												//	Caps Lock
+		Esc: function(){
+				removeOverlay()
+				return false
+			},
 	},
 
+	'.editor:not(.inline-editor-mode)': {
+		title: 'Editor mode.',
+		doc: '',
 
-	// overlay...
-	'.overlay-mode': {
-		title: 'Overlay mode',
-		doc: 'Overlay mode key bindings.',
+		'0': function(){
+				var n = getPageNumber()
+				if(togglePageView('?') == 'on'){
+					setMagazineScale(getPageTargetScale(1))
+				} else {
+					setMagazineScale(getPageTargetScale(PAGES_IN_RIBBON))
+				}
+				setCurrentPage(n)
+			},
+		Esc: '0',
 
-		ignore: [
-			33,													//	PgUp
-			34,													//	PgDown
-			37,													//	Left
-			39,													//	Right
-			36,													//	Home
-			32,													//	Space
-			35,													//	End
-			38,													//	Up
-			40,													//	Down
-		],
+		'=': function(){
+				var n = getPageNumber()
+				setMagazineScale(Math.min(
+						getMagazineScale() * 1.2, 
+						getPageTargetScale(1)))
+				setCurrentPage(n)
+			},
+		'-': function(){
+				var n = getPageNumber()
+				setMagazineScale(Math.max(
+						getMagazineScale() * 0.8, 
+						getPageTargetScale(PAGES_IN_RIBBON*2)))
+				setCurrentPage(n)
+			},
+
+		'O': {
+				// load...
+				// XXX needs testing...
+				'ctrl': function(){
+					showInOverlay('<h1>Open Issue</h1>'+
+						'<input type="file" id="upload" name="file" multiple onchange="handleFileSelect(event)"/>')
+				},
+			},
+		'S': {
+				// save...
+				// XXX needs testing...
+				'ctrl': function(){
+					showInOverlay('<h1>Save Issue</h1>'+
+						'<p>NOTE: this download will not include the actual '+
+						'images. at this point, images should be added manually.</p>'+
+						'<p><a id="data_download" href="#">Download</a></p>')
+
+					// setup the data...
+					$(generateMagazineDownload)
+				},
+			},
+
+		// ?
+		'/': function(){
+				showInOverlay('<h1>Controls</h1>'+
+					'<p>NOTE: this section is a stub.<p>'+
+					'<table width="100%">'+
+						'<tr><td align="right" width="45%"><b> C-O </b></td><td> Load issue from file. </td></tr>'+
+						'<tr><td align="right"><b> C-S </b></td><td> Save issue to file. </td></tr>'+
+						'<tr><td align="right"><b> - / + </b></td><td> Zoom out/in. </td></tr>'+
+						'<tr><td align="right"><b> 0 </b></td><td> Set default zoom level. </td></tr>'+
+					'</table>')
+			},
 	},
-	*/
 
-	// ignore all keys here...
+	// ignore all keys except Esc here...
 	'.inline-editor-mode': {
-		ignore: '*'
-	},
-
-	// everything except overlays...
-	'.viewer:not(.inline-editor-mode)': {
-		title: 'Ribbon and Viewer',
+		title: 'Inline editor mode.',
 		doc: '',
 
-		// navigation...
-		36:		goToMagazineCover,								//	Home
-		219:	36,												//	[
-		35:		goToMagazineEnd,								//	End
-		221:	35,												//	]
-		37:	{
-			'default': prevPage,								//	Right
-			'ctrl': prevArticle,								//	ctrl-Right
-			'shift': prevBookmark								//	shift-Right
-		},
-		188:	37,												//	<
-		39:	{
-			'default': nextPage,								//	Left
-			'ctrl': nextArticle,								//	ctrl-Left
-			'shift': nextBookmark								//	shift-Left
-		},
-		32:	{
-			'default': nextPage,								//	Space
-			'shift': prevPage									//	shift-Space
-		},
-		190:	39,												//	>
+		//ignore: '*'
+		Esc: function(){
+				$(':focus').blur()
+				return false
+			},
+	},
 
-		66: {
-			'default': toggleBookmark,							//	B
-			'ctrl': function(){toggleThemes()},					//	ctrl-B
-		},
+	'.chrome:not(.inline-editor-mode)': {
+		title: 'Global bindings.',
+		doc: '',
 
+		Esc: function(){
+				if(toggleEditor('?') == 'on'){
+					toggleEditor('off')
+				} else {
+					togglePageView('off')
+				}
+			},
+
+		Home: firstPage,
+		End: lastPage, 
+		Left: {
+				default: function(){ prevPage() },
+				shift: prevBookmark,
+				ctrl: prevArticle,
+			},
+		Right: {
+				default: function(){ nextPage() },
+				shift: nextBookmark,
+				ctrl: nextArticle,
+			},
+		Space:	{
+				default: 'Right',
+				shift: 'Left'
+			},
+		//Tab: 'Space',
+		Tab: function(){ return false },
+		Enter: function(){ togglePageView('on') },
 		// combined navigation with actions..
-		38: function(){togglePageView()},						//	Up
-		40: function(){togglePageView()},						//	Down
+		Up: function(){ togglePageView() },
+		Down: function(){ togglePageView() },
 
-		13:	function(){togglePageView('on')},					//	Enter
-		27:	function(){togglePageView('off')},					//	Esc
-	}
-}
+		F: function(){ togglePageFitMode() },
+		B: {
+				default: function(){ toggleBookmark() },
+				ctrl: function(){ toggleThemes() },
+			},
 
+		// XXX this should not be in the production viewer...
+		E: function(){ toggleEditor() },
+	},
+} 
 
 
 /*********************************************************************/
